c4733b67ace70faa3580bae74908368966eb167f
Pawan Gupta <pguptamou@gmail.com>
2026-04-21 13:57:49 +0530

    remove two changes

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 console.log("hii")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 2472c5512308cdb491dedba75e08d662019f8029
-=======
-console.log("hello")
-  console.log("techpragya");
-
->>>>>>> 8d4e36862c7b31ff1bdc317406535d88864946fe
-function genereateRandomfunction(){
-    
-}
